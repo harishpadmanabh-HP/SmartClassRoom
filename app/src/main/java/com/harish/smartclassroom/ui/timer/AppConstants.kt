c643ae8e87bb443f500ac5d1e81ec220bff48b89
harishp@entri.me <harishp@entri.me>
2020-12-05 02:41:25 +0530

    quiz Timer

---
+++ app/src/main/java/com/harish/smartclassroom/ui/timer/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.harish.smartclassroom.ui.timer
+
+
+class AppConstants {
+    companion object {
+        const val ACTION_STOP = "stop"
+        const val ACTION_PAUSE = "pause"
+        const val ACTION_RESUME = "resume"
+        const val ACTION_START = "start"
+    }
+}
